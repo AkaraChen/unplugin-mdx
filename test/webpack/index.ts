46c2ee82c8b8975276200c2d8527635dadd24efd
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-02-13 11:00:14 +0800

    ci: fix

--- test/webpack/index.ts
+++ test/webpack/index.ts
@@ -1,10 +1,10 @@
-import { spawnSync } from "child_process";
-import { readFileSync } from "fs";
-import path from "path";
+import { spawnSync } from 'child_process';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 test('webpack', () => {
   spawnSync('pnpm', ['webpack']);
   const output = path.resolve('./dist/main.js');
   const content = readFileSync(output);
-  expect(content.includes('Hello')).toBeTruthy()
-})
+  expect(content.includes('Hello')).toBeTruthy();
+});
